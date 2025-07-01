69cb30e559a8a8fcb8fd15b012bf8b8fae388277
minho <dnjsalsgh123@gmail.com>
2025-07-01 21:20:57 +0900

    update: port setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  server: {
+    port: 5047
+  },
   plugins: [
     react(),      // React 지원
     tailwindcss() // Vite용 Tailwind 플러그인
