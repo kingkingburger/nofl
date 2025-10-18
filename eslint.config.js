2b1c584fa931063ffcc86c574b65c2ad14e7a5c2
minho <dnjsalsgh123@gmail.com>
2025-10-19 07:18:46 +0900

    fix: batter syntax

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  {ignores: ['dist']},
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
